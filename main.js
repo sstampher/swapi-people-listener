2aa195a33416dca3c8c3042d9aa8a8713d7b0b39
sstampher <stephanierstampher@gmail.com>
2019-05-20 19:07:08 -0700

    got charcter info to display on screen when clicked

--- main.js
+++ main.js
@@ -1,19 +1,32 @@
 
-async function info(){
+async function info(person){
+
     let namesArr = [];
     const response = await fetch('https://swapi.co/api/people/');
     const data = await response.json();
+
+    if(person){
+        for(let i=0;i<data.results.length;i++){
+            if(data.results[i].name === person){
+            const infoCategories = Object.keys(data.results[i]);
+            let infoCategoriesList = infoCategories.map(item => `<li>${item}</li>`)
+            personInfo.innerHTML = `${infoCategoriesList.join(' ')}`
+       }
+    }
+    }                             
+                                    
+
     for(let i=0;i<data.results.length;i++){
     namesArr.push(`<li>${data.results[i].name}</li>`);
     }
     return namesArr.join(' ');
     //people.innerHTML = `${namesArr.join(' ')}`;
 }
-
 info();
 
-let people = document.getElementById('people');
 
+let people = document.getElementById('people');
+let personInfo = document.getElementById('personInfo');
 
 async function displayPeople(){
     return people.innerHTML = `${await info()}`;
@@ -21,3 +34,9 @@ async function displayPeople(){
 
 displayPeople();
 
+async function displayInfo(e){
+    let personName = e.target.innerHTML;
+    console.log(await info(personName));
+}
+
+people.addEventListener('click', displayInfo);
